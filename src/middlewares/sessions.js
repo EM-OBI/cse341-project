febe9ce40f7bebbe28f9d1293885a1440763a58e
EM-OBI <gbassey1@byupathway.edu>
2025-05-30 13:09:45 +0100

    sessions to mongo

--- src/middlewares/sessions.js
+++ src/middlewares/sessions.js
@@ -1,9 +1,15 @@
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 const sessionMiddleware = session({
-  secret: "secret",
+  secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URI,
+    dbName: "cse341-project",
+    ttl: 7 * 24 * 60 * 60
+  })
 });
 
 module.exports = sessionMiddleware;
